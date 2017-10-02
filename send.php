d5219aacfa24d44e6a470b80f426c70c8c417147
Jay Patel <pateljay19991@gmail.com>
2017-10-02 11:56:01 -0700

    commit
    10-02-17

---
+++ send.php
@@ -0,0 +1,20 @@
+<?php 
+	if(isset($_POST['submit'])){
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$phone = $_POST['phone'];
+		$msg = $_POST['msg'];
+
+		$to = "pateljay19991@gmail.com";
+		$subject = "Portfolio Website Message";
+		$message = "Name: ".$name."\n"."Phone: .$phone."\n. "Wrote the Following: "."/n/n.$msg;
+		$headers = "Form: .$email;
+
+		if(mail($to, $subject, $message, $headers)){
+			eacho "<h1>Sent Sucessfully! Thank You"." ".$name.", We will contact you shortly! </h1>";
+		}
+		else{
+			echo "Something went Wrong!";
+		}
+	}	
+?>
